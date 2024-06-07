69d20a13a0124d5c9feb2ad68ac4acae6b26c11e
Flottegurke <justus.dicker@web.de>
2024-06-07 22:14:57 +0200

    recreate backend
    the compleate backend got recreated due to it being setup incorrectly before

---
+++ backend/SAAI-API/src/main/java/org/SAAI/SAAI_API/SaaiApiApplication.java
@@ -0,0 +1,13 @@
+package org.SAAI.SAAI_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaaiApiApplication.class, args);
+	}
+
+}
