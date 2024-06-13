028a6817322d9fd6f53dd807cf2924ad448d7477
Flottegurke <justus.dicker@web.de>
2024-06-13 23:39:53 +0200

    change Method to POST

--- backend/SAAI-API/src/main/java/org/SAAI/SAAI_API/SumController.java
+++ backend/SAAI-API/src/main/java/org/SAAI/SAAI_API/SumController.java
@@ -1,7 +1,7 @@
 package org.SAAI.SAAI_API;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +9,10 @@ import java.util.Map;
 @RestController
 public class SumController {
 
-    @GetMapping("/sum")
-    public Map<String, Integer> sum(@RequestParam int num1, @RequestParam int num2) {
+    @PostMapping("/sum")
+    public Map<String, Integer> sum(@RequestBody Map<String, Integer> request) {
+        int num1 = request.get("num1");
+        int num2 = request.get("num2");
         int result = num1 + num2;
         Map<String, Integer> response = new HashMap<>();
         response.put("sum", result);
