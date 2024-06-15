03c494ad5e64a1c5cf6f5207769013cea1fb3986
dagadeta <dagadeta@web.de>
2024-06-16 00:08:13 +0200

    Add API alerting implementation

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+function sendAlert() {
+    const url = 'http://172.105.89.210:8080';
+    const data = {
+        room: document.getElementById('room').value,
+        description: document.getElementById('description').value,
+    };
+
+    fetch(url + '/alerts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(jsonResponse => {
+            console.log(jsonResponse);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
