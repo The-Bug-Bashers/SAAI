85b41f58417cedfbebe59944df07c65f8dfed32b
Flottegurke <justus.dicker@web.de>
2024-06-21 00:14:50 +0200

    correct timezone

--- backend/SAAI-API/src/main/java/org/SAAI/SAAI_API/SaaiApiApplication.java
+++ backend/SAAI-API/src/main/java/org/SAAI/SAAI_API/SaaiApiApplication.java
@@ -3,10 +3,19 @@ package org.SAAI.SAAI_API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SaaiApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaaiApiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Berlin"));
 	}
 }
