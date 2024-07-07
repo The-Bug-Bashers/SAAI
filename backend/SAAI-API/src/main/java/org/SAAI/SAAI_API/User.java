570d090bf6148f367e07a6aba27df66056140bb9
Flottegurke <justus.dicker@web.de>
2024-07-07 20:07:31 +0200

    fix dependencyes bug

--- backend/SAAI-API/src/main/java/org/SAAI/SAAI_API/User.java
+++ backend/SAAI-API/src/main/java/org/SAAI/SAAI_API/User.java
@@ -1,7 +1,7 @@
 package org.SAAI.SAAI_API;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
