62d1f247985afa6aef1441d48e670aae06fd959d
Flottegurke <justus.dicker@web.de>
2024-09-17 01:52:21 +0200

    fix scc

--- frontend/script.js
+++ frontend/script.js
@@ -72,7 +72,7 @@ function sendAlert() {
     const room = document.getElementById('room').value;
     const description = document.getElementById('description').value;
     fetch(`${url}/api/signalmessage/liveticker?message=New_Alert_in_Room:${encodeURIComponent(room)}_Description:${encodeURIComponent(description)}&password=Baum`)
-    
+
     fetch(url + '/api/alerts', {
         method: 'POST',
         headers: {
