a50a2b4a69a77a03c549f34ca9bcd21211f52758
Flottegurke <justus.dicker@web.de>
2025-01-06 23:12:30 +0100

    refactor apiUrl constant names

--- frontend/resources/scripts/apiUrls.js
+++ frontend/resources/scripts/apiUrls.js
@@ -2,3 +2,5 @@ const saaiApiUrl = "https://saai.wayshare.de:9090/api"; // the current url of th
 const InventoryTrackingAPiUrl = saaiApiUrl + "/coolingpacks"
 const livetickerApiUrl = saaiApiUrl + "/signalmessage/liveticker";
 const usersApiUrl = saaiApiUrl + "/users";
+const timetableApiUrl = saaiApiUrl + "/infoscreen";
+const dayOffApiUrl = saaiApiUrl + "/dayOff";
