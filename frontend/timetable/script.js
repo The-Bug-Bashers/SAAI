7b04853c7b3dd9a1edd9dda965226970c5d8e4d1
Flottegurke <justus.dicker@web.de>
2025-01-18 00:47:26 +0100

    add possibility to display messages
    the /timetable page now automatically opens a info modal with the content of the message URL-component. This is used by the day-off page in order to display a succesmessage after redirection

--- frontend/timetable/script.js
+++ frontend/timetable/script.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     fetchAndDisplayTimetable(document.getElementById("timetableDiv"), true).then(() => {
         document.getElementById('loadingMessage').style.display = 'none';
     });
+
+    const params = new URLSearchParams(window.location.search);
+    if (params.has("message")) {
+        displayNotification(decodeURIComponent(params.get("message")));
+    }
 });
