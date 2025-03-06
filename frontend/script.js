d37eff4da482945edfe25c514349658826678de1
Flottegurke <justus.dicker@web.de>
2025-03-06 22:39:36 +0100

    improve visibility of alerting button

--- frontend/script.js
+++ frontend/script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
             modalContent.innerHTML = `
                 <h2>Alarm senden?</h2><b>Beschreibung: </b><span>${description.value}</span><br><b>Raum: </b><span>${roomDetails}</span>
                 <br>
-                <div id="buttonDiv" style="display: flex">
+                <div id="buttonDiv" style="display: flex" style="margin-top: 0.5em">
                     <button id="returnButton" class="button">Abbrechen</button>
                     <button id="continueButton" style="margin-left: auto" class="button">Alarm senden</button>
                 </div>
