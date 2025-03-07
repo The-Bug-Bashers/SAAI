412ba9a7e53b9eb9cdb651f0b13940cda96aeddb
Flottegurke <justus.dicker@web.de>
2025-03-07 01:16:05 +0100

    add acceptedUsersApiUrl

--- frontend/resources/scripts/apiUrls.js
+++ frontend/resources/scripts/apiUrls.js
@@ -1,4 +1,5 @@
 const saaiApiUrl = "https://saai.wayshare.de:9090/api"; // the current url of the saai api
+
 const InventoryTrackingAPiUrl = saaiApiUrl + "/coolingpacks"
 const livetickerApiUrl = saaiApiUrl + "/signalmessage/liveticker";
 const usersApiUrl = saaiApiUrl + "/users";
@@ -6,3 +7,4 @@ const timetableApiUrl = saaiApiUrl + "/infoscreen";
 const dayOffApiUrl = saaiApiUrl + "/dayOff";
 const messageApiUrl = saaiApiUrl + "/message";
 const alertApiUrl = saaiApiUrl + "/alerts";
+const acceptedUsersApiUrl = alertApiUrl + "/accepted-users";
