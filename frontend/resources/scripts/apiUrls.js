af5ce0161225585640b480b7e6a8338d9d743cc2
Flottegurke <justus.dicker@web.de>
2025-04-20 00:49:13 +0200

    add signalmessage Api Url

--- frontend/resources/scripts/apiUrls.js
+++ frontend/resources/scripts/apiUrls.js
@@ -1,7 +1,8 @@
 const saaiApiUrl = "https://saai.wayshare.de:9090/api"; // the current url of the saai api
 
 const InventoryTrackingAPiUrl = saaiApiUrl + "/coolingpacks"
-const livetickerApiUrl = saaiApiUrl + "/signalmessage/liveticker";
+const signalmessageApiUrl = saaiApiUrl + "/signalmessage";
+const livetickerApiUrl = signalmessageApiUrl + "/liveticker";
 const usersApiUrl = saaiApiUrl + "/users";
 const timetableApiUrl = saaiApiUrl + "/infoscreen";
 const dayOffApiUrl = saaiApiUrl + "/dayOff";
