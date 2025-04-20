994f917bc75fa653f9b6cc355b7f05132765aaf8
Flottegurke <justus.dicker@web.de>
2025-04-20 03:22:18 +0200

    add logging to 404 page

--- frontend/resources/scripts/apiUrls.js
+++ frontend/resources/scripts/apiUrls.js
@@ -1,8 +1,8 @@
-const saaiApiUrl = "https://saai.wayshare.de:9090/api"; // the current url of the saai api
+const saaiApiUrl = "https://saai.wayshare.de:9090/api"; // the current url of the saai api. WARNING if this is changed, also change the url in the 404 page
 
 const InventoryTrackingAPiUrl = saaiApiUrl + "/coolingpacks"
-const signalmessageApiUrl = saaiApiUrl + "/signalmessage";
-const livetickerApiUrl = signalmessageApiUrl + "/liveticker";
+const signalmessageApiUrl = saaiApiUrl + "/signalmessage"; // WARNING if this is changed, also change the url in the 404 page
+const livetickerApiUrl = signalmessageApiUrl + "/liveticker"; // WARNING if this is changed, also change the url in the 404 page
 const usersApiUrl = saaiApiUrl + "/users";
 const timetableApiUrl = saaiApiUrl + "/infoscreen";
 const dayOffApiUrl = saaiApiUrl + "/dayOff";
